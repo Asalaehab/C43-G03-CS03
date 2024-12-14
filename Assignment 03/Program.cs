e22b8e2f7ae95e27e1de3195c68d25649347ce5f
lenovo <lenovo@LAPTOP-7MFDRCOP>
2024-12-14 23:12:17 +0200

    Add project files.

---
+++ Assignment 03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1.Enter number from 
+            int x;
+            bool flag;
+            do
+            {
+                Console.WriteLine("Enter the number : ");
+                flag = int.TryParse(Console.ReadLine(), out x);
+            } while (!flag);
+            Console.WriteLine(x);
+            #endregion
+        }
+    }
+}
